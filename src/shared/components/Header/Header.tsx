b52141ef501ea2aa72fa18b5677d1285fb1f7576
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-16 21:43:28 +0300

    feat: highlite active link and add animation

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,20 +1,38 @@
+"use client";
 import Link from "next/link";
 import Logo from "../Logo/Logo";
 import { headerRoutes } from "@/shared/constants/routes";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const Header = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-4 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-4 h-full text-sm">
           {headerRoutes.map((route) => {
             return (
               <li
                 key={route.path}
-                className="text-white/50 hover:text-white transition"
+                className={clsx(
+                  "hover:text-white flex items-center relative transition",
+                  {
+                    "text-white": pathname === route.path,
+                    "text-white/50": pathname !== route.path,
+                  }
+                )}
               >
                 <Link href={route.path}>{route.name}</Link>
+                {pathname === route.path && (
+                  <motion.div
+                    layoutId="header-active-link"
+                    className="bg-accent h-1 w-full absolute bottom-0"
+                  ></motion.div>
+                )}
               </li>
             );
           })}
